869c05bf6748eca2e7c425303529c8d55a5247d8
Hasret Suna Ozdemır <hasretsunaozdemir@Hasrets-MacBook-Pro.local>
2026-04-18 13:28:51 +0300

    Initial commit

---
+++ Loop/LoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopApp.swift
+//  Loop
+//
+//  Created by Hasret Suna Ozdemır on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
